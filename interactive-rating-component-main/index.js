7c097626738ebf282d415b6f22fe9c28e4cacd21
courtney.province <courtney.province@americaneagle.com>
2022-07-19 09:34:24 -0500

    Add index.js, style.scss, update index.html

---
+++ interactive-rating-component-main/index.js
@@ -0,0 +1,55 @@
+// alert('hi');
+const form = document.querySelector('#form');
+
+let rateButtons = document.getElementsByClassName("rate-btn");
+let rating = 0;
+
+// form.addEventListener('submit', checkRating);
+
+function checkRating(event) {
+    // console.log(event.target);
+    let rateBtns = document.getElementsByClassName('rate-btn');
+    for (let i = 0; i < rateBtns.length; i++) {
+        if (rateBtns[i].checked == true) {
+            // alert('checked!!');
+            rating = rateBtns[i].value;
+            console.log(rating);
+        }
+        else {
+            // alert('not checked');
+        }
+    }
+     return true;
+}
+
+function displayMessage() {
+   
+    document.querySelector('.rating__score').textContent = rating;
+    document.querySelector('.thank-you-card').style.display = "block";
+    document.querySelector('.rating-card').style.display = "none";
+    return false;
+}
+// console.dir(rateButtons);
+
+
+// const check = (e) => {
+//     const form = new FormDataEvent(e.target);
+//     const rating = form.get("rating");
+//     console.log();
+//     return false;
+//     }
+    
+
+// const check = (e) => {
+//     const form = new FormData(e.target);
+//     const rating = form.get("rating");
+//     console.log(rating);
+//     return false;
+// }
+
+
+function check(e) {
+    console.log(e.target);
+    return false;
+}
+
