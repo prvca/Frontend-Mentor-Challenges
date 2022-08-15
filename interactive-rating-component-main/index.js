df1ffc7b8e2df1a03c6c8da694668c6fee191de3
courtney.province <courtney.province@americaneagle.com>
2022-08-15 09:53:52 -0500

    refactor index.js

--- interactive-rating-component-main/index.js
+++ interactive-rating-component-main/index.js
@@ -1,49 +1,30 @@
-
 const form = document.querySelector('#form');
+const rateButtons = document.getElementsByClassName("rate-btn");
 
-let rateButtons = document.getElementsByClassName("rate-btn");
-let rating = 0;
-
-
+//validates form to check if a rating has been selected
 function checkRating(event) {
+    console.log(document.querySelector('input[name="rating"]:checked').value);
     let rateBtns = document.getElementsByClassName('rate-btn');
     for (let i = 0; i < rateBtns.length; i++) {
         if (rateBtns[i].checked == true) {
-            rating = rateBtns[i].value;
-            console.log(rating);
+            return true;
         }
     }
-     return true;
+    return false;    
 }
-
+     
+// calls getRating() and displays the "thank you" message 
 function displayMessage() {
-   
+    const rating = getRating();
     document.querySelector('.rating__score').textContent = rating;
     document.querySelector('.thank-you-card').style.display = "block";
     document.querySelector('.rating-card').style.display = "none";
     return false;
 }
-// console.dir(rateButtons);
 
-
-// const check = (e) => {
-//     const form = new FormDataEvent(e.target);
-//     const rating = form.get("rating");
-//     console.log();
-//     return false;
-//     }
-    
-
-// const check = (e) => {
-//     const form = new FormData(e.target);
-//     const rating = form.get("rating");
-//     console.log(rating);
-//     return false;
-// }
-
-
-function check(e) {
-    console.log(e.target);
-    return false;
+//retrieves the value of the checked radio button
+function getRating() {
+    return document.querySelector('input[name="rating"]:checked').value;
 }
+
 
