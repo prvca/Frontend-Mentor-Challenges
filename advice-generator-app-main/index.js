62983e10e249f756539f00634dbede531a1a071e
courtney.province <courtney.province@americaneagle.com>
2022-08-15 15:03:01 -0500

    create files for advice generator app

---
+++ advice-generator-app-main/index.js
@@ -0,0 +1,17 @@
+const adviceDiv = document.querySelector('.advice');
+let adviceHeading = document.querySelector('.card__heading');
+const button = document.querySelector('.dice');
+button.addEventListener('click', fetchAdvice);
+
+
+function fetchAdvice () {
+    fetch('	https://api.adviceslip.com/advice') 
+        .then((response) => response.json())
+        .then((data) => {
+            adviceDiv.textContent = '"' + data.slip.advice + '"';
+            adviceHeading.textContent = "ADVICE #" + data.slip.id;
+        }
+    );
+}
+
+fetchAdvice();
